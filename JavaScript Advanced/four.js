56204c3fee12c1a1c42e4d5925cbc6ba00cf843f
Sri26sh2003 <srishaashrit9005@gmail.com>
2024-01-07 15:03:49 +0530

    Four

---
+++ JavaScript Advanced/four.js
@@ -0,0 +1,12 @@
+try{
+    let x = undefined;
+    console.log(x[0]);
+    console.log("This will not be executed bcz error has occured");
+}
+catch(err){// we can say what error has occured in the form of arguement
+    console.log("Error is handled in catch block",err);
+}
+finally{
+    console.log("Finally always executes no matter try will execute full or catch will execute full");
+}
+console.log("End of the code");
---
+++ JavaScript Advanced/three.js
@@ -0,0 +1,3 @@
+let x = undefined;
+console.log(x[0]);
+console.log("End of Code");// This statement is not executed before runtime error has occured
